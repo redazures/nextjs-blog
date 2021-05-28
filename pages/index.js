b75f44f67fea9da8cb1dd27157428f7e8f26c283
JamesWu <jwu_nyc@yahoo.com>
2021-05-28 14:45:07 -0400

    fixed

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/post.js";
 import Link from "next/link";
-import Date from "../components/date";
+import Date from "../components/date.js";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
